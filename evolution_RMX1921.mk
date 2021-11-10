ad1c220b6902ee635f7885b894c8a431bce40556
kaderbava <ksbava7325@gmail.com>
2021-11-10 18:55:29 +0000

    RMX1921: Evolution X bringup

--- evolution_RMX1921.mk
+++ evolution_RMX1921.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common AEX stuff
-TARGET_BOOT_ANIMATION_RES := 2280
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Evolution X stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_RMX1921
+PRODUCT_NAME := evolution_RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
