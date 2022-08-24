4e8462bc0da6decdf0000d3e3eeeca31cc93c778
dev-harsh1998 <dev-harsh1998@hotmail.com>
2022-08-24 14:53:54 +0530

    RMX1921:  Enable HWUI_COMPILE_FOR_PERF
    * activates few optimizing compiler flags for HWUI
    
    Signed-off-by: Ij-Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Media
 TARGET_USES_ION := true
 
