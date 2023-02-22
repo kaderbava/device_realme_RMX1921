14a5bc3e5a1351e7d0c07fc3dbb12e04d9c529a4
John Galt <johngaltfirstrun@gmail.com>
2023-02-22 12:59:43 -0500

    RMX1921: utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -488,6 +488,11 @@ PRODUCT_SOONG_NAMESPACES += \
 # SystemUI
 EXCLUDE_SYSTEMUI_TESTS := true
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
