9de80dda79b873a8d6959d91697d5af47ff15550
kaderbava <ksbava7325@gmail.com>
2023-10-29 23:53:17 +0000

    RMX1921: Remove some gapps
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos SoundAmplifierPrebuilt GoogleCamera AdaptiveVPNPrebuilt AmbientStreaming CalendarGooglePrebuilt CarrierLocation CarrierMetrics DevicePolicyPrebuilt DiagnosticsToolPrebuilt FilesPrebuilt ScribePrebuilt Showcase Tycho
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Drive Maps Photos MeetPrebuilt PrebuiltGmail GoogleCamera AmbientStreaming
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
