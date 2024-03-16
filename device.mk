359d0deac52c2b38083d61cea941ab7969a52676
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-16 01:24:14 +0000

    RMX1921: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -523,6 +523,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
     libnl \
     libwfdaac_vendor \
     vendor.qti.hardware.display.config-V5-ndk
