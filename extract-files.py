f2f886c60c49fb1295b34014d0a69b82555bf95d
LuK1337 <priv.luk@gmail.com>
2024-10-31 14:15:18 +0100

    RMX1921: Shim WFD with libgui_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     ('vendor/lib/hw/android.hardware.health@2.0-impl-2.1-qti.so', 'vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-qti.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
+    'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V3-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
