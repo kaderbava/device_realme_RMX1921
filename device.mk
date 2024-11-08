32c6b8868eff02fbf5af49b8cf49b404410551d2
kaderbava <ksbava7325@gmail.com>
2024-11-08 04:24:46 +0000

    RMX1921: Cleanup dead targets
    Offending entries:
    vndk_package
    com.android.vndk.current.on_vendor
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -516,10 +516,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_VNDK_VERSION := current
 PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
-PRODUCT_PACKAGES += \
-    vndk_package \
-    com.android.vndk.current.on_vendor
-
 # Wallet
 PRODUCT_PACKAGES += \
     QuickAccessWallet
