3d641b3b17bed1344afdc417a17a01a04b7174aa
mikeNG <mikeNG@users.noreply.github.com>
2024-11-26 13:37:37 +0000

    RMX1921: Shim widevine with libcrypto_shim
    The following change
    google/boringssl@e202e51
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -159,7 +159,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
