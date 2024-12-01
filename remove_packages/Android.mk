3897d4482b173b323e7de3cc7cb91d2a4e769843
kaderbava <ksbava7325@gmail.com>
2024-12-01 19:39:56 +0000

    RMX1921: Adapt to new gms
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt CalendarGooglePrebuilt FilesPrebuilt Photos ScribePrebuilt DevicePolicyPrebuilt RecorderPrebuilt GoogleCamera SwitchAccessPrebuilt VoiceAccessPrebuilt DreamlinerPrebuilt DreamlinerUpdater HelpRtcPrebuilt SafetyHubPrebuilt PlayAutoInstallConfig talkback Updater
+LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt_v4.7.638126989 CalendarGooglePrebuilt FilesPrebuilt Photos ScribePrebuilt_v7.0.633113815 DevicePolicyPrebuilt-v10052480 RecorderPrebuilt GoogleCamera SwitchAccessPrebuilt_1.15.0.629986523 VoiceAccessPrebuilt SafetyHubPrebuilt talkback arcore-1.42
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
