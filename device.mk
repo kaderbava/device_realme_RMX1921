e8ab428b0fd165d3de85d9c78f73f45d44be8293
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-10 15:36:30 -0400

    RMX1921: Migrate away from TARGET_RECOVERY_DEVICE_MODULES
    * Dead as of QPR2.
    
    Change-Id: I05bcf246266b89fa7da964f7aa75870b5a63a3b9
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -352,6 +352,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp_whitelist_org.device.Realmeparts.xml:system/etc/permissions/privapp_whitelist_org.device.Realmeparts.xml
 
+# Recovery
+PRODUCT_PACKAGES += \
+    libinit_RMX1921.recovery
+
 # Remove Unwanted Packages
 PRODUCT_PACKAGES += \
     RemovePackages
