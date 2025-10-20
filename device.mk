0a522cfe7a0c46e744e49f9ff6ea5e43fabf14ae
kyasu <kyasu.dev@gmail.com>
2025-10-20 21:11:44 +0600

    RMX1921: Disable thumbnail block model in stagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -218,6 +218,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
