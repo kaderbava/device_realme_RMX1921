e4bedc2516caa3fc39b56ab31419f2378d4ab200
Guidix <guidixm@gmail.com>
2025-10-20 22:28:20 +0600

    RMX1921: Silent some spammy logging
    Add spammy log tags for non-eng build variants.
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -193,6 +193,20 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,livedisplay_sdm,enable_dm,false)
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    AccessPersistence \
+    AiAiEcho \
+    AppOps \
+    AppOpService\
+    HWUI \
+    ResilientAtomicFile
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=S)
+endif
+
 # Media
 PRODUCT_PACKAGES += \
     libminijail \
