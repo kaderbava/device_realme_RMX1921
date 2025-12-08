3f55071bcb124d385c83a7cc4b64a78d52e8adff
AnierinB <anierin@evolution-x.org>
2025-12-08 09:06:13 +0600

    RMX1921: Move to PreferenceFragmentCompat
    PreferenceFragment was deprecated in API level 28.
    
    https://developer.android.com/jetpack/androidx/releases/preference#1.1.0
    
    Change-Id: Ie8a4b04bd588afbbbc5882349ddbc05f988f80bf
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- RealmeParts/src/org/device/RealmeParts/speaker/ClearSpeakerFragment.java
+++ RealmeParts/src/org/device/RealmeParts/speaker/ClearSpeakerFragment.java
@@ -26,14 +26,14 @@ import android.os.Looper;
 import android.util.Log;
 
 import androidx.preference.Preference;
-import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
 import org.device.RealmeParts.R;
 
 import java.io.IOException;
 
-public class ClearSpeakerFragment extends PreferenceFragment implements
+public class ClearSpeakerFragment extends PreferenceFragmentCompat implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "ClearSpeakerFragment";
@@ -46,7 +46,7 @@ public class ClearSpeakerFragment extends PreferenceFragment implements
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.clear_speaker_settings);
+        setPreferencesFromResource(R.xml.clear_speaker_settings, rootKey);
 
         mClearSpeakerPref = findPreference(PREF_CLEAR_SPEAKER);
         mClearSpeakerPref.setOnPreferenceChangeListener(this);
