7ebf6b8745a89836e1bc4c1ee0c97367c7e9721d
Bruno Nartins <bgcngm@gmail.com>
2025-12-08 14:59:23 +0600

    RMX1921: Fully migrate to AndroidX
    Change-Id: I873a18de1b7382ee9dc0be4da0ff53192a99b1db
    Signed-off-by: Ij-Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- RealmeParts/src/org/device/RealmeParts/RealmeParts.java
+++ RealmeParts/src/org/device/RealmeParts/RealmeParts.java
@@ -17,7 +17,6 @@
 */
 package org.device.RealmeParts;
 
-import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -33,6 +32,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.util.Log;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreferenceCompat;
