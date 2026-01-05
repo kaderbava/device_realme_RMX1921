93e01d790927d8d8b2b891a1050f69844f7cdabd
kaderbava <ksbava7325@gmail.com>
2026-01-05 14:01:55 +0000

    RMX1921: {RealmeParts,RealmeDoze}: Adapt our features while fully migrate to AndroidX
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- RealmeParts/src/org/device/RealmeParts/audio/SoundControlSettingsActivity.java
+++ RealmeParts/src/org/device/RealmeParts/audio/SoundControlSettingsActivity.java
@@ -17,7 +17,7 @@
 package org.device.RealmeParts.audio;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -29,7 +29,9 @@ public class SoundControlSettingsActivity extends CollapsingToolbarBaseActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         Fragment fragment = getSupportFragmentManager().findFragmentById(com.android.settingslib.collapsingtoolbar.R.id.content_frame);
         if (fragment == null) {
