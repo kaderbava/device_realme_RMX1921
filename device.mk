24eb1b4f24ba4252a6d5d421dda9d1ace6d2ee88
me-cafebabe <me-cafebabe@users.noreply.github.com>
2026-02-26 19:56:53 +0000

    RMX1921: Disable VINTF kernel requirements
    Change-Id: I12a75936976078a29fa0d15e24ced97d1ad9e4c1
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/realme/RMX1921/RMX1921-vendor.mk)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Bluetooth
 PRODUCT_PACKAGES += \
