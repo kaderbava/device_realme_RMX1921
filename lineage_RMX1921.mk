05abdd82e0a86d18b003933e770d544fbc907446
kaderbava <ksbava7325@gmail.com>
2026-03-22 19:22:47 +0000

    RMX1921: Axion-ify!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- lineage_RMX1921.mk
+++ lineage_RMX1921.mk
@@ -4,12 +4,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common LineageOS stuff
+# Inherit some common AxionOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Axion-ify!
+AXION_CAMERA_REAR_INFO := 64,8,2,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := Kader_Bava
+AXION_PROCESSOR := Qualcomm_Snapdragon_712_AIE
+PERF_ANIM_OVERRIDE := true
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# Lineage prebuilts
+ifneq ($(WITH_GMS),true)
+TARGET_INCLUDES_LOS_PREBUILTS := true
+endif
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
