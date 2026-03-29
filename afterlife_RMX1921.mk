d4675648fb76d35e0d3ab30c357584d1691a3b47
kaderbava <ksbava7325@gmail.com>
2026-03-29 21:08:52 +0000

    RMX1921: Disable blurs again
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- afterlife_RMX1921.mk
+++ afterlife_RMX1921.mk
@@ -9,7 +9,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_ENABLE_BLUR := true
 PERF_ANIM_OVERRIDE := true
 $(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
