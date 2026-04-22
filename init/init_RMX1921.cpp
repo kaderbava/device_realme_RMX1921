0e2cc4f22df14390ffb79767a025c0f5180e5169
Ij Israfil <israfilkhan494@gmail.com>
2026-04-22 19:59:44 +0000

    RMX1921 : Remove LMKD property overrides
    * We have switched to slmk.
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- init/init_RMX1921.cpp
+++ init/init_RMX1921.cpp
@@ -63,11 +63,6 @@ void load_dalvikvm_properties() {
     property_override("dalvik.vm.heapstartsize", "8m");
     property_override("dalvik.vm.heapgrowthlimit", "256m");
     property_override("dalvik.vm.heapmaxfree", "16m");
-    property_override("ro.lmk.psi_complete_stall_ms", "600");
-    property_override("ro.lmk.psi_partial_stall_ms", "130");
-    property_override("ro.lmk.swap_free_low_percentage", "20");
-    property_override("ro.lmk.thrashing_limit", "55");
-    property_override("ro.lmk.thrashing_limit_decay", "37");
   }
   property_override("dalvik.vm.heaptargetutilization", "0.75");
   property_override("dalvik.vm.heapsize", "512m");
